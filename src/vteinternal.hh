43e8a01be69629a0092c8bb1448ecb926f3acca2
Egmont Koblinger <egmont@gmail.com>
2018-08-22 11:54:15 +0200

    RingView architecture first steps

--- src/vteinternal.hh
+++ src/vteinternal.hh
@@ -24,6 +24,7 @@
 #include "vtetypes.hh"
 #include "vtedraw.hh"
 #include "reaper.hh"
+#include "bidi.hh"
 #include "ring.hh"
 #include "buffer.h"
 #include "parser.hh"
@@ -601,6 +602,7 @@ public:
 
         /* BiDi parameters outside of ECMA and DEC private modes */
         guint m_bidi_rtl  : 1;
+        vte::base::RingView m_ringview;
 
 public:
 
